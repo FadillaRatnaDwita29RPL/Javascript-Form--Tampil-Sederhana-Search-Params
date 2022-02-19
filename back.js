55e9f06d8a4dfb2696c1c03833a128790a2361d7
FadillaRatnaDwita29RPL <fadilladwita20@gmail.com>
2022-02-19 16:06:57 +0700

    first commit

---
+++ back.js
@@ -0,0 +1,5 @@
+function ktp() {
+    let nik = document.getElementById("nik").value;
+    let nama = document.getElementById("nama").value;
+    let alamat = document.getElementById("alamat").value;
+}
